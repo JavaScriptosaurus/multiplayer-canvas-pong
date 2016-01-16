d6516927fbbb9db0a4dcf9d8dc015c405eab532e
Brody McKee <mrmckeb@hotmail.com>
2016-01-16 17:07:51 +1100

    Added methods, and testing code in app.js

--- src/client/app.js
+++ src/client/app.js
@@ -1,6 +1,51 @@
 import canvasManager from './utils/canvas';
 
 const canvas = document.getElementsByTagName('canvas')[0];
-const ourCanvas = canvasManager.init(canvas, 600, 400);
+const canvasRenderer = canvasManager.getRenderer(canvas, {width: 600, height: 400});
 const ourRectangleTest = canvasManager.createRectangle(50, 50, 10, 10, 'blue');
-ourRectangleTest.draw();
+const ourOtherRectangleTest = canvasManager.createRectangle(100, 50, 100, 150, 'rgba(255, 155, 55, 1)');
+
+canvasRenderer.addElements(ourRectangleTest, ourOtherRectangleTest);
+
+setInterval(function () {
+    ourOtherRectangleTest.move({ x: 0.2, y: -0.1 });
+}, 10);
+
+let interval;
+document.addEventListener('keydown', function (event) {
+    const pressedKey = event.keyCode;
+    const keyCodes = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    let movement = {
+        x: 0,
+        y: 0
+    };
+
+    switch (keyCodes[pressedKey]) {
+        case 'left':
+            movement.x = -1;
+            break;
+        case 'right':
+            movement.x = 1;
+            break;
+        case 'up':
+            movement.y = -1;
+            break;
+        case 'down':
+            movement.y = 1;
+            break;
+    };
+
+    clearInterval(interval);
+    interval = setInterval(function () {
+        ourRectangleTest.move(movement);
+    }, 1);
+});
+document.addEventListener('keyup', function () {
+    clearInterval(interval);
+});
--- src/client/utils/canvas.js
+++ src/client/utils/canvas.js
@@ -5,8 +5,7 @@
 
 class Shape {
 
-    constructor (ctx, width, height, x, y, color) {
-        this.ctx = ctx;
+    constructor (width, height, x, y, color) {
         this.width = width;
         this.height = height;
         this.x = x;
@@ -14,11 +13,26 @@ class Shape {
         this.color = color;
     }
 
+    setContext (context) {
+        this.ctx = context;
+    }
+
     draw () {
-        this.ctx.beginPath();
-        this.ctx.rect(this.x, this.y, this.width, this.height);
-        this.ctx.fillStyle = this.color;
-        this.ctx.fill();
+        if (this.ctx) {
+            this.ctx.beginPath();
+            this.ctx.rect(this.x, this.y, this.width, this.height);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+        }
+    }
+
+    move (dimensions) {
+        if (dimensions.x) {
+            this.x += dimensions.x;
+        }
+        if (dimensions.y) {
+            this.y += dimensions.y;
+        }
     }
 
 }
@@ -31,17 +45,55 @@ class Circle extends Shape {
     // TODO
 }
 
+function CanvasRenderer (canvas, dimensions) {
+    // Set up canvas and dimensions.
+    const ctx = canvas.getContext('2d');
+    canvas.width = dimensions.width;
+    canvas.height = dimensions.height;
+
+    // Renderable elements
+    let elementsToRender = [];
+
+    function renderCanvas () {
+        // Clear whole canvas.
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // For each element to render, call draw.
+        elementsToRender.forEach(function (element) {
+            if (element.draw instanceof Function) {
+                element.draw();
+            }
+        });
+    };
+
+    // Start rendering.
+    const requestAnimationFrame = window.requestAnimationFrame;
+    (function renderLoop () {
+        renderCanvas();
+        requestAnimationFrame(renderLoop);
+    })();
+
+    return {
+
+        addElements: function (...elements) {
+            // TODO: Check if element is in the list?
+            elements.forEach(function (element) {
+                element.setContext(ctx);
+                elementsToRender.push(element);
+            });
+        }
+
+    };
+}
+
 export default {
 
-    init: function (canvas, canvasWidth, canvasHeight) {
-        this.ctx = canvas.getContext('2d');
-        canvas.width = canvasWidth;
-        canvas.height = canvasHeight;
-        return this;
+    getRenderer: function (canvas, dimensions) {
+        return CanvasRenderer(canvas, dimensions);
     },
 
     createRectangle: function (width, height, x, y, color) {
-        return new Rectangle(this.ctx, width, height, x, y, color);
+        return new Rectangle(width, height, x, y, color);
     }
 
 }
